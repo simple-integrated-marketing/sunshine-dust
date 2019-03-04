40eb23ebebfc2ad32c1345017f42e30e23b5ecb1
Ben Rogerson <info@benrogerson.com.au>
2019-03-04 15:03:03 +1030

    finish initial work

--- README.md
+++ README.md
@@ -1,40 +1,65 @@
-# ☀️🤯 Sunshine Dust
+# Weather Wonka
 
-Sunshine Dust displays data from the [Willy Weather API](https://www.willyweather.com.au/info/api.html) in your site or app.
+Weather Wonka displays data from the [Willy Weather API](https://www.willyweather.com.au/info/api.html) in your site or app.
+
+[![default visuals](https://raw.githubusercontent.com/simple-integrated-marketing/weather-wonka/master/screenie.gif)](https://raw.githubusercontent.com/simple-integrated-marketing/weather-wonka/master/screenie.gif)
 
 ## Getting started
 
 ### 1. Import plugin and configure it
 
-By default, Sunshine Dust creates a html structure with the [BEM](http://getbem.com/introduction/]) methodology.<br/>
-If you'd like to adjust the markup then opt for option b.
+The default template creates [BEM](http://getbem.com/introduction/]) based markup.<br/>
+If you'd like to adjust the markup then choose the 'b' option below.
 
 #### a) Use the default templates
 
 ```js
-import SunshineDust from 'sunshine-dust';
+import WeatherWonka from 'weather-wonka';
 
-// The element where we'll add the weather
+// The element where the html will be added
 const el = document.querySelector('[data-weather]');
 
 // The url for the weather data api
 const apiUrl =
-    'https://api.willyweather.com.au/v2/[YOUR_TOKEN]/locations/9495/weather.json?forecasts=weather';
+    'https://api.willyweather.com.au/v2/[YOUR_WILLY_WEATHER_KEY]/locations/8672/weather.json?forecasts=weather';
 
 // Start the plugin
-new SunshineDust(el, apiUrl, { days: 1 });
+new WeatherWonka(el, apiUrl, { days: 1 });
 ```
 
 This config will produce the following markup:
 
 ```html
-<div class="weather weather--1day">
-    <div class="weather__day">
-        <div class="weather__icon">wi-day-sunny</div>
-        <div class="weather__min">Min 26</div>
-        <div class="weather__max">Max 41</div>
+<a href="#" data-weather="">
+    <div class="weather weather--1day">
+        <div class="weather__item">
+            <div class="weather__title">
+                Monday
+            </div>
+            <div class="weather__details">
+                <div class="weather__icon" data-label="Mostly sunny">
+                    <svg
+                        class="weather__svg"
+                        xmlns:xlink="http://www.w3.org/1999/xlink"
+                        aria-hidden="true"
+                    >
+                        <use xlink:href="#weather-icon--day-cloudy"></use>
+                    </svg>
+                </div>
+                <div class="weather__min-max">
+                    <div class="weather__temp weather__temp--min">
+                        <abbr class="weather__label" title="Minimum">Min</abbr>
+                        <span class="weather__value">17</span>
+                    </div>
+                    <div class="weather__temp weather__temp--max">
+                        <abbr class="weather__label" title="Maximum">Max</abbr>
+                        <span class="weather__value">31</span>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
-</div>
+</a>
 ```
 
 #### b) Define custom templates
@@ -42,36 +67,42 @@ This config will produce the following markup:
 If you'd like complete control of the produced markup then you can create custom templates like this:
 
 ```js
-import SunshineDust from "sunshine-dust";
+import WeatherWonka from "weather-wonka";
 
-// The template for the day(s) container
+// The template for the day(s)
 const templateContainer = data => (`
     <div class="${data.blockName}">${data.content}</div>
 `);
 
 // The template for each day
 const templateDay = data => (`
-    <div class="${data.blockName}__day">
+    <div class="${data.blockName}__item">
+        <div class="${data.blockName}__day">${data.dayName}</div>
         <div class="${data.blockName}__icon">${data.icon}</div>
+        <div class="${data.blockName}__precis">${data.precis}</div>
         <div class="${data.blockName}__max">${data.max}</div>
         <div class="${data.blockName}__min">${data.min}</div>
     </div>
 `);
 
-// The element where we'll add the weather
+// The element where the html will be added
 const el = document.querySelector('[data-weather]');
 
 // The url for the weather data api
 const apiUrl =
-  "https://api.willyweather.com.au/v2/[YOUR_TOKEN]/locations/9495/weather.json?forecasts=weather";
+  "https://api.willyweather.com.au/v2/[YOUR_WILLY_WEATHER_KEY]/locations/8672/weather.json?forecasts=weather";
+
+// Customise the day names
+const dayNameDefaults = ['Sun','Mon','Tue','Wed','Thu','Fri','Sat'];
 
 // Start the plugin
-new SunshineDust(el, apiUrl, {
+new WeatherWonka(el, apiUrl, {
     {
         blockName: 'weather',
         days: 7,
         templateContainer,
         templateDay,
+        dayNameDefaults,
     }
 });
 ```
@@ -87,27 +118,23 @@ In this case, we'll be linking to a weather page when the weather is selected an
 
 ### 3. Style the markup
 
-#### a) Use the default styles
-
-In your SCSS, import the styles:
+Either use the default scss styles as a starting point:
 
 ```scss
-@import 'sunshine-dust/styles/main';
+@import 'weather-wonka/src/styles';
 ```
+Or write custom styles for to suit the markup.
 
-#### b) Define custom styles
+## Icons
 
-You can also go fully custom with your styling.
-No need to import anything, just style as you want.
+We've included [some icons to get you started](https://raw.githubusercontent.com/simple-integrated-marketing/weather-wonka/master/icon-examples.zip) but you can define your own custom icons.<br>
+The default templates assume you're using a SVG icon sprite within your markup.
+
+## Demo
+
+Add your Willy Weather API key within `weather-wonka/src/demo.html` then run `npm start`.
+
 
 ## Credits
 
-Created by [@benrogerson](https://twitter.com/benrogerson) @ [Simple](<[Simple](https://simple.com.au)>)
-
-Swiff has been agency battletested by [Simple](https://simple.com.au) who specialise in Craft CMS websites.
-
-## TODO:
-
--   [ ] Build icons into a sprite
--   [ ] Add basic styles
--   [ ] Transpile SCSS to CSS
+Weather Wonka is created by [@benrogerson](https://twitter.com/benrogerson) @ [Simple](<[Simple](https://simple.com.au)>).
---
+++ src/index.js
@@ -0,0 +1,196 @@
+// Weather Wonka
+export default class WeatherWonka {
+    constructor(el, apiUrl, config) {
+        this.el = el;
+        this.apiUrl = apiUrl;
+
+        // Backup and remove the element html
+        this.beforeInnerContent = el.innerHTML;
+        this.el.innerHTML = '';
+
+        // Set the default variables
+        const defaults = {
+            blockName: 'weather',
+            days: 1,
+            iconMap: new Map(iconMapDefaults),
+            dayNames,
+            templateContainer,
+            templateDay,
+        };
+        // Combine configs
+        this.config = Object.assign({}, defaults, config);
+
+        // Fix days if misconfigured
+        if (this.config.days > 7 || this.config.days < 1) this.config.days = 7;
+
+        this.isStarted = false;
+        this.init();
+    }
+
+    async init() {
+        if (this.isStarted || !!this.el.length) return;
+
+        // Get the weather data
+        const weatherData = await getRequest(this.apiUrl);
+        if (!weatherData || !weatherData.forecasts)
+            return console.error('Error connecting to the apiUrl');
+
+        // Get the source day data from the weather forecast
+        const { days: weatherDataDays } = weatherData.forecasts.weather;
+
+        // Get the number of days to display
+        const { days } = this.config;
+
+        // Get the data for the number of days specified
+        const dayData = getDayData(weatherDataDays, days);
+
+        // Generate the markup for each of the days
+        const dayHtml = dayData.map(day => {
+            return this.config.templateDay(
+                {
+                    ...this.config,
+                    ...day,
+                    dayName: this.config.dayNames[
+                        new Date(day.dateTime).getDay()
+                    ],
+                    icon: this.config.iconMap.get(day.precisCode),
+                },
+                dayData.length > 1 ? 'li' : 'div'
+            );
+        });
+
+        // Generate the container markup and include the day markup
+        const weatherHtml = this.config.templateContainer(
+            {
+                ...this.config,
+                blockModifier: `${this.config.blockName}--${days}day${
+                    days > 1 ? 's' : ''
+                }`,
+                content: dayHtml.join('\n'),
+            },
+            days > 1 ? 'ul' : 'div'
+        );
+
+        // Add the markup within our element
+        this.el.innerHTML = weatherHtml;
+
+        this.isStarted = true;
+    }
+
+    // Restore the original view
+    destroy() {
+        if (!this.isStarted) return;
+        this.el.innerHTML = this.beforeInnerContent;
+        this.isStarted = false;
+    }
+}
+
+// Define icon mappings
+// ['precisCode', 'iconName']
+const iconMapDefaults = [
+    ['fine', 'weather-icon--day-sunny'],
+    ['mostly-fine', 'weather-icon--day-cloudy'],
+    ['high-cloud', 'weather-icon--day-cloudy-high'],
+    ['partly-cloudy', 'weather-icon--day-cloudy'],
+    ['mostly-cloudy', 'weather-icon--day-cloudy'],
+    ['cloudy', 'weather-icon--cloudy'],
+    ['overcast', 'weather-icon--cloudy'],
+    ['shower-or-two', 'weather-icon--showers'],
+    ['chance-shower-fine', 'weather-icon--day-showers'],
+    ['chance-shower-cloud', 'weather-icon--showers'],
+    ['drizzle', 'weather-icon--sprinkle'],
+    ['few-showers', 'weather-icon--showers'],
+    ['showers-rain', 'weather-icon--rain'],
+    ['heavy-showers-rain', 'weather-icon--rain'],
+    ['chance-thunderstorm-fine', 'weather-icon--day-storm-showers'],
+    ['chance-thunderstorm-cloud', 'weather-icon--storm-showers'],
+    ['chance-thunderstorm-showers', 'weather-icon--storm-showers'],
+    ['thunderstorm', 'weather-icon--thunderstorm'],
+    ['chance-snow-fine', 'weather-icon--day-snow-wind'],
+    ['chance-snow-cloud', 'weather-icon--snow'],
+    ['snow-and-rain', 'weather-icon--rain-mix'],
+    ['light-snow', 'weather-icon--snow'],
+    ['snow', 'weather-icon--snow'],
+    ['heavy-snow', 'weather-icon--snow'],
+    ['wind', 'weather-icon--strong-wind'],
+    ['frost', 'weather-icon--snowflake-cold'],
+    ['fog', 'weather-icon--fog'],
+    ['hail', 'weather-icon--hail'],
+    ['dust', 'weather-icon--dust'],
+];
+
+// Define daynames
+const dayNames = [
+    'Sunday',
+    'Monday',
+    'Tuesday',
+    'Wednesday',
+    'Thursday',
+    'Friday',
+    'Saturday',
+];
+
+// The template for the day(s) container
+const templateContainer = (data, wrapTag) => `
+    <${wrapTag} class="${data.blockName} ${data.blockModifier}">${
+    data.content
+}</${wrapTag}>
+`;
+
+// The template for each day
+const templateDay = (data, wrapTag) => `
+<${wrapTag} class="${data.blockName}__item">
+    <div class="${data.blockName}__title">
+        ${data.dayName}
+    </div>
+    <div class="${data.blockName}__details">
+        <div class="${data.blockName}__icon" data-label="${data.precis}">
+            <svg class="${
+                data.blockName
+            }__svg" xmlns:xlink="http://www.w3.org/1999/xlink" aria-hidden="true">
+                <use xlink:href="#${data.icon}" />
+            </svg>
+        </div>
+        <div class="${data.blockName}__min-max">
+            <div class="${data.blockName}__temp ${data.blockName}__temp--min">
+                <abbr class="${
+                    data.blockName
+                }__label" title="Minimum">Min</abbr>
+                <span class="${data.blockName}__value">${data.min}</span>
+            </div>
+            <div class="${data.blockName}__temp ${data.blockName}__temp--max">
+                <abbr class="${
+                    data.blockName
+                }__label" title="Maximum">Max</abbr>
+                <span class="${data.blockName}__value">${data.max}</span>
+            </div>
+        </div>
+    </div>
+</${wrapTag}>
+`;
+
+// Fetch a response and convert to JSON
+const getRequest = async url => {
+    let request;
+    await fetch(url)
+        .then(response => {
+            request = response.json();
+        })
+        .catch(error => {
+            return console.error('Error:', error, url);
+        });
+    return request;
+};
+
+// Get the simplified day data from the Willy Weather json
+const getDayData = (days, dayLimit) =>
+    days
+        .map((day, index) => {
+            const firstResult = day.entries && day.entries[0];
+            return index + 1 <= dayLimit ? { ...firstResult } : null;
+        })
+        .filter(Boolean);
+
+// Make it available on the window object
+// if that floats your boat
+window.WeatherWonka = WeatherWonka;
